59bb0e0c51663177ecf199896c93348d7c36ef90
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-02-01 21:12:31 +0500

    configurations has been added

---
+++ UzTexGroupV2.Infrastructure/Configurations/FactoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UzTexGroupV2.Domain.Entities;
+
+namespace UzTexGroupV2.Infrastructure.Configurations;
+
+public class FactoryConfiguration : IEntityTypeConfiguration<Factory>
+{
+    public void Configure(EntityTypeBuilder<Factory> builder)
+    {
+        builder.ToTable(nameof(Factory))
+            .HasKey(factory => factory.Id);
+
+        builder.Property(factory => factory.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasOne(factory => factory.Company)
+            .WithMany(company => company.Factories)
+            .HasForeignKey(factory => factory.CompanyId);
+
+        builder.HasOne(factory => factory.Address)
+            .WithOne()
+            .HasForeignKey<Factory>(factory => factory.AddressId);
+
+    }
+}
---
+++ UzTexGroupV2.Infrastructure/Configurations/JobConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UzTexGroupV2.Domain.Entities;
+
+namespace UzTexGroupV2.Infrastructure.Configurations;
+
+public class JobConfiguration : IEntityTypeConfiguration<Job>
+{
+    public void Configure(EntityTypeBuilder<Job> builder)
+    {
+        builder.ToTable(nameof(Job))
+            .HasKey(job => job.Id);
+
+        builder.Property(job => job.Salary)
+            .IsRequired();
+
+        builder.Property(job => job.WorkTime)
+            .IsRequired();
+
+        builder.HasOne(job => job.Factory)
+            .WithMany(factory => factory.Jobs)
+            .HasForeignKey(job => job.FactoryId);
+
+        builder.HasMany(job => job.JobNames)
+            .WithOne()
+            .HasForeignKey(dict => dict.Id);
+
+        builder.HasMany(job => job.Descriptions)
+            .WithOne()
+            .HasForeignKey(dict => dict.Id);
+
+    }
+}
