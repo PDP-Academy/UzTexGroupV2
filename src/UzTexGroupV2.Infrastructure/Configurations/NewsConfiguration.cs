0aa8d36fc441a962b767a71a1b9c99b49ca3a841
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2023-02-04 15:57:00 +0500

    News entity configuratsiya qilindi

--- src/UzTexGroupV2.Infrastructure/Configurations/NewsConfiguration.cs
+++ src/UzTexGroupV2.Infrastructure/Configurations/NewsConfiguration.cs
@@ -8,31 +8,28 @@ public class NewsConfiguration : IEntityTypeConfiguration<News>
 {
     public void Configure(EntityTypeBuilder<News> builder)
     {
-        // builder.ToTable(nameof(News));
-        //
-        // builder.HasKey(news => news.Id);
-        //
-        // builder
-        //     .Property(news => news.Date)
-        //     .IsRequired();
-        //
-        // builder
-        //     .Property(news => news.TitleId) 
-        //     .IsRequired();
-        //     
-        // builder
-        //     .Property(news => news.DescriptionId)
-        //     .IsRequired();
-
-        // builder
-        //     .HasMany(news => news.Titles)
-        //     .WithOne()
-        //     .IsRequired();
-        //
-        // builder
-        //     .HasMany(news => news.Descriptions)
-        //     .WithOne()
-        //     .IsRequired();
+        builder.ToTable(nameof(News));
+        
+        builder.HasKey(news => news.Id);
+        
+        builder
+            .Property(news => news.Date)
+            .IsRequired();
+        
+        builder
+            .HasMany(news => news.Titles)
+            .WithOne()
+            .HasForeignKey(dictionary => dictionary.Id)
+            .HasPrincipalKey(news => news.TitleId)
+            .IsRequired();
+            
+        builder
+            .HasMany(news => news.Descriptions)
+            .WithOne()
+            .HasForeignKey(dictionary => dictionary.Id)
+            .HasPrincipalKey(news => news.DescriptionId)
+            .IsRequired();
+        
 
         // builder
         //     .Property(news => news.Images)
---
+++ src/UzTexGroupV2.Infrastructure/Migrations/20230204104941_Initial.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UzTexGroupV2.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Region = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    District = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PostalCode = table.Column<short>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TitleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DescriptionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserRole = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Factory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NameTextId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AddressId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Factory", x => x.Id);
+                    table.UniqueConstraint("AK_Factory_NameTextId", x => x.NameTextId);
+                    table.ForeignKey(
+                        name: "FK_Factory_Address_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Factory_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewsImages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NewId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NewsId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NewsImages_News_NewsId",
+                        column: x => x.NewsId,
+                        principalTable: "News",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JobNameId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DescriptionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WorkTime = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FactoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Job_Factory_FactoryId",
+                        column: x => x.FactoryId,
+                        principalTable: "Factory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Application",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AddressId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ApplicationMessage = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Application", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Application_Address_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Application_Job_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Job",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LanguageDictionary",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LanguageCode = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    NewsId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    NewsId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LanguageDictionary", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LanguageDictionary_Factory_Id",
+                        column: x => x.Id,
+                        principalTable: "Factory",
+                        principalColumn: "NameTextId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LanguageDictionary_Job_Id",
+                        column: x => x.Id,
+                        principalTable: "Job",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LanguageDictionary_Job_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Job",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LanguageDictionary_News_NewsId",
+                        column: x => x.NewsId,
+                        principalTable: "News",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LanguageDictionary_News_NewsId1",
+                        column: x => x.NewsId1,
+                        principalTable: "News",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Application_AddressId",
+                table: "Application",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Application_JobId",
+                table: "Application",
+                column: "JobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Factory_AddressId",
+                table: "Factory",
+                column: "AddressId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Factory_CompanyId",
+                table: "Factory",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Job_FactoryId",
+                table: "Job",
+                column: "FactoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LanguageDictionary_JobId",
+                table: "LanguageDictionary",
+                column: "JobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LanguageDictionary_NewsId",
+                table: "LanguageDictionary",
+                column: "NewsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LanguageDictionary_NewsId1",
+                table: "LanguageDictionary",
+                column: "NewsId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsImages_NewsId",
+                table: "NewsImages",
+                column: "NewsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Application");
+
+            migrationBuilder.DropTable(
+                name: "LanguageDictionary");
+
+            migrationBuilder.DropTable(
+                name: "NewsImages");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Job");
+
+            migrationBuilder.DropTable(
+                name: "News");
+
+            migrationBuilder.DropTable(
+                name: "Factory");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+        }
+    }
+}
