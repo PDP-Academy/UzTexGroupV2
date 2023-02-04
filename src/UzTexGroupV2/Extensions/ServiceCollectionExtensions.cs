99d18c6d879ea0955bccfb8a195b50e53f32453a
Elchin Uralov <elchinuralov07@gmail.com>
2023-02-04 15:09:01 +0500

    program cs ga db context qo'shildi

---
+++ src/UzTexGroupV2/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UzTexGroupV2.Infrastructure.DbContexts;
+
+namespace UzTexGroupV2.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDbContexts(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContextPool<UzTexGroupDbContext>(obj =>
+        {
+            obj.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"),
+                obj => obj.EnableRetryOnFailure());
+        }); 
+
+        return services;
+    }
+}
