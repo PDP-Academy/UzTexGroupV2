43d628fd4ff26feeb1455f2f90e14ef731b4eafc
Elchin Uralov <elchinuralov07@gmail.com>
2023-02-05 16:14:10 +0500

    Added Services class

--- src/UzTexGroupV2/Controllers/UserController.cs
+++ src/UzTexGroupV2/Controllers/UserController.cs
@@ -5,9 +5,14 @@ using UzTexGroupV2.Infrastructure.Repositories;
 
 namespace UzTexGroupV2.Controllers;
 
-public class UserController : LocalizedController
+public class UserController : LocalizedControllerBase
 {
     public UserController(LocalizedUnitOfWork localizedUnitOfWork) : base(localizedUnitOfWork)
     {
+    }
+
+    public List<int> GetData()
+    {
+        return new List<int>(){1, 2, 3};
     }
 }
