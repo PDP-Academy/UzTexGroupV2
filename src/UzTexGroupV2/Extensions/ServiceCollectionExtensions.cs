bdec8677240f28b804e6a70883985899b13d1672
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2023-02-05 16:18:47 +0500

    Merge conflict solved

--- src/UzTexGroupV2/Extensions/ServiceCollectionExtensions.cs
+++ src/UzTexGroupV2/Extensions/ServiceCollectionExtensions.cs
@@ -14,7 +14,7 @@ public static class ServiceCollectionExtensions
         {
             obj.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"),
                 obj => obj.EnableRetryOnFailure());
-        }); 
+        });
 
         return services;
     }
