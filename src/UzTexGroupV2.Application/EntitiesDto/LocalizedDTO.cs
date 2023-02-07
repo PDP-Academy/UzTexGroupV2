31ba3c1b8b426d7596ee94a680f6a4eafffb7bba
TohirjonIbnAlisher <ibnalisher7379@gmail.com>
2023-02-07 17:28:10 +0500

    annotation has been added to localizedDto

--- src/UzTexGroupV2.Application/EntitiesDto/LocalizedDTO.cs
+++ src/UzTexGroupV2.Application/EntitiesDto/LocalizedDTO.cs
@@ -1,6 +1,11 @@
-﻿namespace UzTexGroupV2.Application.EntitiesDto;
+﻿using System.ComponentModel.DataAnnotations;
+using UzTexGroupV2.Application.EntitiesDto.Company;
+
+namespace UzTexGroupV2.Application.EntitiesDto;
 
 public record LocalizedDTO
 {
+    [Required(ErrorMessage = $"{nameof(LocalizedDTO)}  berilishi majburiy")]
+    [StringLength(5, ErrorMessage = "Language 5 ta belgida oshmasligi kerak")]
     public string LanguageCode { get; set; }
 }
