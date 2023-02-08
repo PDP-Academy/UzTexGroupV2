fd595633554f91cfc7007841bb20f0ec62cabede
Elchin Uralov <elchinuralov07@gmail.com>
2023-02-08 15:35:52 +0500

    .

--- src/UzTexGroupV2/Controllers/UserController.cs
+++ src/UzTexGroupV2/Controllers/UserController.cs
@@ -1,15 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using UzTexGroupV2.Application.Services;
+using UzTexGroupV2.Domain.Entities;
+using UzTexGroupV2.Filters;
 using UzTexGroupV2.Infrastructure.Repositories;
+using UzTexGroupV2.Model;
 
 namespace UzTexGroupV2.Controllers;
 
+[Controller]
+[Route("/{langCode}/[controller]")]
 public class UserController : LocalizedControllerBase
 {
-    public UserController(LocalizedUnitOfWork localizedUnitOfWork) : base(localizedUnitOfWork)
+    public UserController(LocalizedUnitOfWork localizedUnitOfWork, UserService service) : base(localizedUnitOfWork)
     {
     }
 
+    [HttpGet]
+    [ResponeFilter]
     public List<int> GetData()
     {
-        return new List<int>(){1, 2, 3};
+        Console.WriteLine(base.Request.RouteValues["langCode"]);
+
+        return new List<int>() { 1, 2, 3 };
     }
 }
